78661a90ef80f4169cf9991cff223db86b0248d7
Jonathan Saylor <jsaylor525@gmail.com>
2018-03-12 20:33:30 -0500

    Initial Commit
    * Working on using delegates for unit test.

---
+++ async_programming_in_c_sharp/AsyncDelgateDemo/AsyncDelgateDemo/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingDelgates
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private void DoWork()
+        {
+            Debug.WriteLine("Hello World");
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+        }
+
+        delegate void DoWorkDelegate();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+            DoWorkDelegate m = new DoWorkDelegate(DoWork);
+            IAsyncResult ar = m.BeginInvoke(null, null);
+            // do more
+            m.EndInvoke(ar);
+        }
+    }
+}
