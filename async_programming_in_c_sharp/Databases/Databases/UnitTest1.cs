2b22a71f53c57361dfec45c660b5f152fbcfc32b
Jonathan Saylor <jsaylor525@gmail.com>
2018-03-12 21:45:19 -0500

    SQL database example
    * Can't test because I don't have a SQL server up currently.

---
+++ async_programming_in_c_sharp/Databases/Databases/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace Databases
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_DB_Sync()
+        {
+            string connectionString;
+            #region Assign connectionString
+            connectionString = @"data source=localhost;initial catalog=AdventureWorks2008R2;integrated security=SSPI";
+            #endregion
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand(sqlSelect, sqlConnection);
+                var callback = new AsyncCallback(DataAvailable);
+                var ar = sqlCommand.BeginExecuteReader(callback, sqlCommand);
+
+                ar.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        private static void DataAvailable(IAsyncResult ar)
+        {
+            var sqlCommand = ar.AsyncState as SqlCommand;
+            using (var reader = sqlCommand.EndExecuteReader(ar))
+            {
+                while (reader.Read())
+                {
+                    var data = reader[0].ToString();
+                }
+            }
+        }
+    }
+}
