3eb1c5f29c5c67afdfb4adfa89e061d63a4011cf
Jonathan Saylor <jsaylor525@gmail.com>
2018-03-12 21:13:54 -0500

    Adding quick networking example from 2.b
    * Async networking with begin/end
    * Basic url page grabbed on background thread, with a async wait.
    * Potentially useful for get data and not interrupting current thread processing/ui rendering.

---
+++ async_programming_in_c_sharp/Networking/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+
+namespace Networking
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string url = "http://www.delsink.com";
+
+        [TestMethod]
+        public void Test_Download_DelsinkCOM()
+        {
+            var httpRequestInfo = HttpWebRequest.CreateHttp(url);
+            var callback = new AsyncCallback(HttpResponseAvailable);
+            var ar = httpRequestInfo.BeginGetResponse(callback, null);
+
+            ar.AsyncWaitHandle.WaitOne();
+        }
+
+        private static void HttpResponseAvailable(IAsyncResult ar)
+        {
+            var httpRequestInfo = ar.AsyncState as HttpWebRequest;
+            var httpResponseInfo = httpRequestInfo.EndGetResponse(ar) as HttpWebResponse;
+
+            var rs = httpResponseInfo.GetResponseStream();
+            using (var sr = new StreamReader(rs))
+            {
+                var webpage = sr.ReadToEnd();
+            }
+        }
+    }
+}
