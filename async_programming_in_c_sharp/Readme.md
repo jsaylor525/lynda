9e2e490abbbf5dfe3655cd028c9925b55f469d7e
Jonathan Saylor <jsaylor525>
2018-03-28 22:07:52 -0500

    Adding Section 4.1 playground

---
+++ async_programming_in_c_sharp/Readme.md
@@ -0,0 +1,26 @@
+# Asynchronous Programming in C# 
+
+Notes from the lynda.com course on asychronous programming using C# and Microsoft
+Visual Studio 2017 Community.
+
+## Introduction
+
+## Section 1 - Background
+
+### 1.1 What is asynchronous
+
+### 1.2 ...
+
+## Section 2 - I/O
+
+## Section 3 - Windows Forms
+
+## Section 4 - Hard Multithreading
+
+### 4.1 - Threads
+
+Introducing the System.Threading library to create and run threads. Creating Solution Threads to demo library.
+
+* What Dangerous
+* All review from Parallel Programming in college...
+* Shared memory issues
---
+++ async_programming_in_c_sharp/Threads/Threads/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of Main");
+
+            var t1 = new Thread(new ThreadStart(DoWork));
+            t1.Start();
+
+            var t2 = new Thread(new ThreadStart(DoWork));
+            t2.Start();
+
+            var t3 = new Thread(new ThreadStart(DoWork));
+            t3.Start();
+
+            Console.WriteLine("End of Main");
+        }
+
+        private static Random rnd = new Random();
+        private static int total = 10;
+
+        private static void DoWork()
+        {
+            int myTotal = total;
+            Thread.Sleep(rnd.Next(1, 500));
+            total = myTotal + 1;
+            Console.WriteLine("Current Thread ID: {0} says {1}", Thread.CurrentThread.ManagedThreadId, total);
+            //Thread.Sleep(1000);
+        }
+    }
+}
