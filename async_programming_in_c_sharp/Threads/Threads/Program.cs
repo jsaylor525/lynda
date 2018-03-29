cbbfbbba13979dacab94b28afd3ff47cad5a7605
Jonathan Saylor <jsaylor525>
2018-03-28 22:29:45 -0500

    Creating a critical section
    * Did some extra timing to see how expansive locking is... it's very expensive.

--- async_programming_in_c_sharp/Threads/Threads/Program.cs
+++ async_programming_in_c_sharp/Threads/Threads/Program.cs
@@ -12,29 +12,53 @@ namespace Threads
         static void Main(string[] args)
         {
             Console.WriteLine("Start of Main");
-
-            var t1 = new Thread(new ThreadStart(DoWork));
+            int starttime = DateTime.Now.Millisecond;
+            var t1 = new Thread(new ThreadStart(AddToList));
             t1.Start();
 
-            var t2 = new Thread(new ThreadStart(DoWork));
+            var t2 = new Thread(new ThreadStart(AddToList));
             t2.Start();
 
-            var t3 = new Thread(new ThreadStart(DoWork));
+            var t3 = new Thread(new ThreadStart(AddToList));
             t3.Start();
 
-            Console.WriteLine("End of Main");
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            int stoptime = DateTime.Now.Millisecond;
+
+
+            Console.WriteLine("End of Main, execution took {0}ms to add {1} items to the list",stoptime-starttime, numList.Count);
         }
 
-        private static Random rnd = new Random();
-        private static int total = 10;
-
-        private static void DoWork()
+        private static object myLock = new object();
+        private static List<int> numList = new List<int>();
+        private static void AddToList()
         {
-            int myTotal = total;
-            Thread.Sleep(rnd.Next(1, 500));
-            total = myTotal + 1;
-            Console.WriteLine("Current Thread ID: {0} says {1}", Thread.CurrentThread.ManagedThreadId, total);
-            //Thread.Sleep(1000);
+            // Much fast to here, about 9ms execution time
+            lock (myLock)
+            {
+            for(int ix = 0; ix < 100000; ix++)
+            {
+            // Much slower to lock here, about 28ms execution time
+            //lock (myLock)
+            //{
+                    numList.Add(ix);
+            }
+            }
         }
+
+        //private static Random rnd = new Random();
+        //private static int total = 10;
+
+        //private static void DoWork()
+        //{
+        //    int myTotal = total;
+        //    Thread.Sleep(rnd.Next(1, 500));
+        //    total = myTotal + 1;
+        //    Console.WriteLine("Current Thread ID: {0} says {1}", Thread.CurrentThread.ManagedThreadId, total);
+        //    //Thread.Sleep(1000);
+        //}
     }
 }
